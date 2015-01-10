2c3105a7718410ef27e3d9bd5e87abc933d39a29
sehlstrom <alexander@sehlstrom.se>
2015-01-10 15:37:41 +0100

    Create WebServer.h
    Web server header file.

---
+++ WebServer.h
@@ -0,0 +1,54 @@
+/*
+ * @flie WebServer.h
+ * @version 1.0
+ *
+ * @section License
+ * Copyright (C) 2015, Alexander Sehlström
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * This file is part of the Arduino ElFi project.
+ */
+ 
+#ifndef WEB_SERVER_H
+#define WEB_SERVER_H
+
+#include "Cosa/INET/HTTP.hh"
+#include "Cosa/IOStream.hh"
+
+// HTML end of line
+#define CRLF "\r\n"
+
+/**
+ * Subclass of HTTP::Server, a server request handler class. Implements project specific
+ * on_request() function to produce response to HTTP requests.
+ */
+class WebServer : public HTTP::Server
+{
+  public:
+    /**
+     * Default constructor.
+     */
+    WebServer() {}
+
+    /**
+     * Override of the HTTP::Server:on_request() member function. Displays
+     * the ElFi web application controll and communicats with the rest of the
+     * controll system.
+     * @param[in] page iostream for response.
+     * @param[in] method http request method string.
+     * @param[in] path resource path string.
+     * @param[in] query possible query string.
+     */
+    virtual void on_request(IOStream& page, char* method, char* path, char* query);
+};
+
+#endif
